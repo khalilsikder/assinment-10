b175c2a69e0c809e83025f393e65fee49839867a
khalilsikder <khalil.sikder03@gmail.com>
2022-04-17 12:23:17 +0600

    setup picture

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
+        <div className='about-me'>
+            <h1>khalilur Rahman</h1>
+            <p>I am khalilur rahman.I am a student of programming hero.Future want to be web developer.now doing hardwork daily.i know industry is the key to success.so trying day by day.</p>
+            
+                <img src="https://i.ibb.co/n3WNyx8/khalil.jpg" alt="" />
             
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className='header-links'>
         <Link to='/'>Home</Link>
         <Link to ='/contact'>Contact</Link>
         <Link to ='/service'>Service</Link>
         <Link to ='/about'>About</Link>
         <Link to ='/blog'>Blog</Link>
-        <Link to ='/about'>About</Link>
         <Link to='/login'>Login</Link>
 
         </div>
