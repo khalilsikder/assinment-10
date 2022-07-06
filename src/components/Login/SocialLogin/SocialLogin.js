ce1f9ec154f98b685753a04a412bd04982409c32
khalilsikder <khalil.sikder03@gmail.com>
2022-07-06 17:49:52 +0600

    change size

--- src/components/Login/SocialLogin/SocialLogin.js
+++ src/components/Login/SocialLogin/SocialLogin.js
@@ -20,7 +20,7 @@ const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
    
     return (
         <div>
-            <div className='d-flex align-items-center'>
+            <div className='social d-flex align-items-center mx-25'>
               <div style={{height:'1px'}} className='bg-primary w-25 align-items-center'></div>
                 <p className='mt-2 px-2'>or</p>
                 <div style={{height:'1px'}} className='bg-primary w-25 align-items-center'></div>
